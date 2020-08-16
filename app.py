9db4e8d54797cdbd7f36f55a7887b1850d9b0623
Amr Sekilly <amr@pelcro.com>
2020-08-16 17:48:56 +0200

    Read the data from the DB

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://amrelsekilly@localhost:5432/postgres'
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f'<Todo ID: {self.id}, title: {self.title}>'
+
+
+db.create_all()
 
 
 @app.route('/')
 def index():
-    data = [{
-        'title': 'Todo 1'
-    }, {
-        'title': 'Todo 2'
-    }, {
-        'title': 'Todo 3'
-    }]
+    data = Todo.query.all()
     return render_template('index.html', data=data)
