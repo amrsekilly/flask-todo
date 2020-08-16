81f547ac3c85ffda3147fc17263bf07ede7c850e
Amr Sekilly <amr@pelcro.com>
2020-08-16 17:52:39 +0200

    Changed the DB

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://amrelsekilly@localhost:5432/postgres'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://amrelsekilly@localhost:5432/todoapp'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
