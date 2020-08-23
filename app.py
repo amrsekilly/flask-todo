01af676ee1d8346e048b424ec15bf79f6a365cce
Amr Sekilly <amr@pelcro.com>
2020-08-23 15:46:57 +0200

    Added  the todolist model

--- app.py
+++ app.py
@@ -15,9 +15,17 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
     completed = db.Column(db.Boolean, nullable=False)
+    list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'))
 
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
+
+
+class TodoList(db.Model):
+    __tablename__ = 'todolists'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    todos = db.relationship('Todo', backref='list', lazy=True)
 
 
 @app.route('/todos/<todo_id>', methods=['DELETE'])
